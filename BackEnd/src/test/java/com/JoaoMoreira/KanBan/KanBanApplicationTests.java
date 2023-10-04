45987b2c3767a75dccfd0fa7539e6f8f78a78750
JoaoVMoreira <jvittormoreira@outlook.pt>
2023-10-03 23:48:07 -0300

    Iniciando projeto Spring e instalando dependências

---
+++ BackEnd/src/test/java/com/JoaoMoreira/KanBan/KanBanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JoaoMoreira.KanBan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanBanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
